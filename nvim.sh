d1eeaf5a08cd6e7d7fbb081080dd8b33e14ada4c
bengraham-B <grahamben7@gmail.com>
2024-04-15 21:27:40 +0200

    added github URL

--- nvim.sh
+++ nvim.sh
@@ -1,8 +1,15 @@
 # Pull git repo
+clone https://github.com/bengraham-B/nvim-config.git
 
 
 
 # remove nvim file
+rm ~/.config/nvim
 
+# Make the nvim dir in ~/.config
+mkdir ~/.config/nvim
 
 # save contents of nvim file to ~/.config/nvim
+mv lua ~/.config/nvim 
+mv init.lua ~/.config/nvim
+mv laz-lock.json ~/.config.json
