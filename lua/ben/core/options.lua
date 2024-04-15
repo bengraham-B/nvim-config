0f9c56807686884a783b99c0bafbf86e68125002
bengraham-B <grahamben7@gmail.com>
2024-04-15 21:21:39 +0200

    Added bash script to move nvim files to ~/.config

---
+++ lua/ben/core/options.lua
@@ -0,0 +1,27 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.number = true
+
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true                   
+opt.wrap = false
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = false
+
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
---
+++ nvim.sh
@@ -0,0 +1,8 @@
+# Pull git repo
+
+
+
+# remove nvim file
+
+
+# save contents of nvim file to ~/.config/nvim
